48b0443b95c75489734a04f50265a147a65518b2
Denis Hurskiy <denis.gurskiy@gmail.com>
2024-03-07 10:52:26 +0200

    make build

---
+++ index.d0767162.js
@@ -0,0 +1,2 @@
+window.addEventListener("hashchange",function(){"menu"===window.location.hash.substring(1)?document.body.classList.add("page__body--menu--open"):document.body.classList.remove("page__body--menu--open")});
+//# sourceMappingURL=index.d0767162.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,8 @@
+"use strict";
+window.addEventListener("hashchange", ()=>{
+    const targetId = window.location.hash.substring(1);
+    if (targetId === "menu") document.body.classList.add("page__body--menu--open");
+    else document.body.classList.remove("page__body--menu--open");
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
